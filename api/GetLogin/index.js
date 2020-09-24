70efe58c7493a972d0e313e83052271b38014dac
Luca Bolognese <lucabol@microsoft.com>
2020-09-24 21:20:32 +0200

    print used

--- api/GetLogin/index.js
+++ api/GetLogin/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
     context.log(user)
 
     if(user)
-        return utils.htmlResponse('<a class="button is-primary" href="/.auth/logout"><strong>Logout</strong></a>')
+        return utils.htmlResponse('<a class="button is-primary" href="/.auth/logout"><strong>Logout</strong></a>' + user.userDetails)
     else
         return utils.htmlResponse('<a class="button is-primary" href="/.auth/login/aad"><strong>Login</strong></a>')
 }
