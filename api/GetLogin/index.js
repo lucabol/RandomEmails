be93a819ada714c003dc7b1688bac574bb52331d
Luca Bolognese <lucabol@microsoft.com>
2020-09-25 16:37:42 +0200

    change sign in

--- api/GetLogin/index.js
+++ api/GetLogin/index.js
@@ -14,7 +14,7 @@ module.exports = async function (context, req) {
     else
         return U.hr(html`
         <a class="button is-danger" href="/.auth/login/aad">
-            <span class="icon is-small"><i class="fab fa-microsoft"></i></span>
+            <span class="icon is-small"><i class="fas fa-sign-in-alt"></i></span>
             <span><strong>Login</strong></span>
         </a>`)
 }
